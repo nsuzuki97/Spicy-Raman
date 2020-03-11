925da76dd6f09a87f5acbfdab5627f4cee6199f5
Nozomi Suzuki <diegocat@uw.edu>
2020-03-11 00:21:26 -0700

    made .py files functionalized notebooks

---
+++ Augmentation.py
@@ -0,0 +1,37 @@
+from numpy import expand_dims
+from keras.preprocessing.image import load_img
+from keras.preprocessing.image import img_to_array
+from keras.preprocessing.image import ImageDataGenerator
+from matplotlib import pyplot
+from matplotlib import image
+%matplotlib agg
+import os
+
+
+def augmentation(name):
+    # load the image
+    img = load_img('Pre_Augmented_Raman_Pictures/' + name )
+    # convert to numpy array
+    data = img_to_array(img)
+    # expand dimension to one sample
+    samples = expand_dims(data, 0)
+    # create image data augmentation generator
+    datagen = ImageDataGenerator(zoom_range=[0.5,1.0])
+    # prepare iterator
+    it = datagen.flow(samples, batch_size=1)
+    # generate samples and plot
+    for i in range(9):
+        batch = it.next()
+        # convert to unsigned integers for viewing
+        current_image = batch[0].astype('uint8')
+        # plot raw pixel data
+        fig = pyplot.imshow(current_image)
+        #imwrite(fig, 'Post_Augmented_Raman_Pictures/1')
+        image.imsave('Post_Augmented_Raman_Pictures/' + str(i) + name  , current_image)
+
+def full_augmentation(folder_name):
+    with os.scandir(folder_name) as entries:
+        for entry in entries:
+            name = entry.name
+            augmentation(name)
+            print(name)
---
+++ preprocessing.py
@@ -0,0 +1,31 @@
+import numpy as np
+#import matplotlib.pyplot as plt
+%matplotlib agg
+import pandas as pd
+import os
+
+
+def graph_and_save (data, name):
+    y = data.iloc[:, 1]
+    x = data.iloc[:, 0]
+    fig = plt.figure()
+    ax = plt.subplot(111)
+    ax.plot(x, y, label='$y = numbers')
+    ax.legend()
+    fig.savefig('Pre_Augmented_Raman_Pictures/' + name)
+    #print(type(fig))
+    plt.close('all')
+    
+    
+def preprocessing(big_folder)
+    os.listdir()
+    with os.scandir(big_folder) as entries:
+        for entry in entries:
+            foldername = entry.name
+            print(foldername)
+            with os.scandir(big_folder + foldername) as entries:
+                for entry in entries:
+                    filename = entry.name
+                    data = pd.read_csv(big_folder + '/' + foldername + '/' + filename , sep='\s+')
+                    name = filename + "_" + foldername + ".png"
+                    graph_and_save(data, name)

09c25bc99907ffc6b01038bfd508550204ad5f5b
Julia Wang <xironw@uw.edu>
2020-03-17 14:37:55 -0700

    assert

---
+++ model_train_and_validate/py_files/test_pic_class.py
@@ -0,0 +1,16 @@
+import os
+import pic_class as pc
+import shutil
+
+def test_picture_classification():
+    source = os.getcwd()
+    source2 = os.path.dirname(source) + '/Post_Augmented_Raman_Pictures'
+    pc.picture_classification(source2)
+    with os.scandir(source2) as entries:
+        for entry in entries:
+            foldername = entry.name
+            pnumber = 0
+            if foldername.endswith(".jpg") or foldername.endswith(".png"):
+                pnumber = pnumber + 1
+    assert pnumber is not 0,'There is no pictures in the' + source2
+    

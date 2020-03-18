1c9107410745f0e83e2a7a8f2ca851f77f06ebc8
Julia Wang <xironw@uw.edu>
2020-03-17 20:00:37 -0700

    assert4

--- model_train_and_validate/py_files/test_preprocessing.py
+++ model_train_and_validate/py_files/test_preprocessing.py
@@ -11,14 +11,13 @@ def test_graph_and_save():
     big_folder = os.path.dirname(source) + '/Raman_Data'
     data = pd.read_csv(big_folder + '/' + 'fluorescent' + '/' + '20191004 Set3Sample3_10a 1_.txt' , sep='\s+')
     name = 'fluorescent' + "_" + '20191004 Set3Sample3_10a 1_.txt' + ".png"
-    pp.graph_and_save(data, name)
     assert len(data) is not 2, 'the dataframe needs to have two columns'
     assert isinstance(data, pd.DataFrame) == True, 'you need to put a Dataframe inside'
+    pp.graph_and_save(data, name)
     
 def test_preprocessing():
     source = os.getcwd()
     big_folder = os.path.dirname(source) + '/Raman_Data'
-    pp.preprocessing(big_folder)
     with os.scandir(big_folder) as entries:
         for entry in entries:
             foldername = entry.name
@@ -31,3 +30,4 @@ def test_preprocessing():
                     if filename.endswith(".txt"):
                         pnumber = pnumber + 1
     assert pnumber is not 0,'There is no txt file in the' + subig_folder
+    pp.preprocessing(big_folder)
